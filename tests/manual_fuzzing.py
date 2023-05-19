e065c62c8b8d7a446a6b3894761db86825655054
MaximG <maxim.gonnissen@student.uantwerpen.be>
2023-05-19 02:58:33 +0200

    Add single run manual test method

--- tests/manual_fuzzing.py
+++ tests/manual_fuzzing.py
@@ -15,7 +15,11 @@ sequences = [
     "SE"
 ]
 
-if __name__ == '__main__':
+
+def run_all() -> None:
+    """
+    Performs fuzzing for combinations of all test maps & sequences.
+    """
     config = json.load(open("./data/config/config.json", "r"))
 
     config["seed"] = "test"
@@ -29,4 +33,32 @@ if __name__ == '__main__':
             map_string = open(maps_directory + input_map, "r").read()
             fuzzer.run_jpacman(map_string, sequence, f"input_map: {input_map}")
 
-    fuzzer.generate_report()
+    fuzzer.generate_report(name="manual_fuzzing")
+
+
+def run_single(map_name: str, sequence: str) -> None:
+    """
+    Performs fuzzing for a single map & sequence.
+    :param map_name: Input map name.
+    :param sequence: Input sequence.
+    """
+    config = json.load(open("./data/config/config.json", "r"))
+
+    config["seed"] = "test"
+
+    fuzzer = Fuzzer(config=config)
+
+    try:
+        map_string = open(maps_directory + map_name, "r").read()
+    except FileNotFoundError:
+        print("Map not found.")
+        return
+
+    fuzzer.run_jpacman(map_string, sequence, f"input_map: {map_name}")
+
+    fuzzer.generate_report(name="manual_fuzzing")
+
+
+if __name__ == '__main__':
+    # run_all()
+    run_single("test_order.map", "SE")
